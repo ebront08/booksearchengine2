7a17b965d1bbbaeaecf6f35fc72cc6da8b8b4ade
ebront08 <ebront08@gmail.com>
2021-11-12 11:03:52 -0500

    files

---
+++ client/build/precache-manifest.7a1bdb41c8d678d36261458e2cc7c46f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5a8094e59a305e69e9a9116d039049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8820cbff370d9fa290e",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "fafb7827a5ce60c41af7",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "c8820cbff370d9fa290e",
+    "url": "/static/js/2.2c1769e3.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.2c1769e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafb7827a5ce60c41af7",
+    "url": "/static/js/main.b6607b2d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
